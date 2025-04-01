910ae2253d18bf0059e83369cbe41dbc23f5f3ed
Aljaž <126414096+ShadowAly@users.noreply.github.com>
2025-04-01 08:25:14 +0000

    added flask startup with index.html

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#blablabla
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    ime = "Aljaž Žiga"
+    return render_template("index.html", ime = ime)
+
+
+app.run(debug = True)
